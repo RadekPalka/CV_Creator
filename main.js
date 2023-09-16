4cbf65187032bcdc75f1832a23741178fdb0d1a8
Radoslaw Palka <radekpalka@gmail..com>
2023-09-16 16:47:33 +0200

    create index.html

---
+++ main.js
@@ -0,0 +1,31 @@
+// const pdf = document.querySelector(".pdf")
+// // html2pdf(pdf)
+// const nameInput = document.querySelector("#name-input")
+// const phoneNumberInput = document.querySelector("#phone-number-input")
+// const emailInput = document.querySelector("#email-input")
+// const name = document.querySelector("#name")
+// const phoneNumber = document.querySelector("#phone-number")
+// const email = document.querySelector("#email")
+// const btn = document.querySelector("button")
+
+// const createPdfDocument = () =>{
+//   name.textContent = nameInput.value
+//   phoneNumber.textContent = phoneNumberInput.value
+//   email.textContent = emailInput.value
+//   const filename = `${nameInput.value} CV.pdf`
+//   const option ={
+//     filename
+//   }
+//   html2pdf().set(option).from(pdf).save();
+
+//   // pdf.style.display = "none"
+// }
+
+// btn.addEventListener("click", createPdfDocument)
+const button = document.querySelector("button")
+
+const addExperience = () =>{
+  window.open()
+}
+
+button.addEventListener("click", addExperience)
